38475da100071f89d94f338d3820fe2a0ba90abc
Mahmoud <mahmoud@localhost.localdomain>
2021-01-24 11:37:59 -0500

    Added progress bar

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <wait.h>
+#include <unistd.h>
+
 
 #define READ_END 0
 #define WRITE_END 1
 
+long fileLength(FILE *file);
+void progressBar(int rate);
+
 int main(int argc, char **argv){
 
+	int status;
+	int pid;
+	int parent2Child[2];
+	int child2Parent[2];
+	
 	if (argc < 2) {
 		printf("Please enter a file name.\nUsage: ./programName <file_name>\n");
 		return -1;
@@ -20,21 +30,13 @@ int main(int argc, char **argv){
 
 	}
 
-	int status;
-	int pid;
-	int fdP2C[2];
-	int fdC2P[2];
-	
-
 	FILE *file = fopen(argv[1], "r");
-    fseek(file, 0, SEEK_END);
-    long length = ftell(file);
+	long length = fileLength(file);
+
     char sent[length+1];
 	char rcvd[length+1];
-    fseek(file, 0, SEEK_SET);
 
-    
-	if (pipe(fdP2C) == -1 || pipe(fdC2P) == -1){
+	if (pipe(parent2Child) == -1 || pipe(child2Parent) == -1){
 		printf("Error creating pipe!\n");
 		return 1;
 	}
@@ -46,49 +48,56 @@ int main(int argc, char **argv){
 	}
 
 	else if (pid > 0) {
-		printf("Parent Thread:\n");	
-		close(fdP2C[READ_END]);
+		printf("Process 1 is reading the input file '%s' now ...\n", argv[1]);	
+		close(parent2Child[READ_END]);
     	fread(sent, 1, length, file);
     	fclose(file);
     	sent[length] = '\0';
-		write(fdP2C[WRITE_END], sent, strlen(sent)+1);
-		close(fdP2C[WRITE_END]);
-		printf("Parent waiting for child to complete:\n");
+    	printf("Process 1 starts sending the data to Process 2 ...\n");	
+		write(parent2Child[WRITE_END], sent, strlen(sent)+1);
+		close(parent2Child[WRITE_END]);
+		// printf("Parent waiting for child to complete:\n");
 		pid = wait(&status);
-		printf("Parent continues after child finishes:\n");
+		// printf("Parent continues after child finishes:\n");
 
-		close(fdC2P[WRITE_END]);
+		close(child2Parent[WRITE_END]);
 		int numberOfWordsFromChild;
-		read(fdC2P[READ_END], &numberOfWordsFromChild, sizeof(numberOfWordsFromChild));
-		printf("Number of words in the file (child -> parent): %d\n", numberOfWordsFromChild);
-		close(fdC2P[READ_END]);
+		read(child2Parent[READ_END], &numberOfWordsFromChild, sizeof(numberOfWordsFromChild));
+		printf("Process 1: The total number of words is %d.\n", numberOfWordsFromChild);
+		close(child2Parent[READ_END]);
 	}
 	
 	else {
-		printf("Chid Thread:\n");	
-		close(fdP2C[WRITE_END]);
-		read(fdP2C[READ_END], rcvd, length);
-		int numberOfWords = wordCount(rcvd);
-		printf("Child is Counting the words:\n");
-		close(fdP2C[READ_END]);
+		close(parent2Child[WRITE_END]);
+		read(parent2Child[READ_END], rcvd, length);
+		printf("Process 2 finishes receiving data from Process 1 ...\n");	
+		printf("Process 2 is counting the words now ...\n");	
+		int numberOfWords = wordCount(rcvd, length);
+		close(parent2Child[READ_END]);
 
-		close(fdC2P[READ_END]);
-		write(fdC2P[WRITE_END], &numberOfWords, sizeof(numberOfWords));
-		close(fdC2P[WRITE_END]);
+		close(child2Parent[READ_END]);
+		printf("Process 2 is sending the results back to Process 1 ...\n");	
+		write(child2Parent[WRITE_END], &numberOfWords, sizeof(numberOfWords));
+		close(child2Parent[WRITE_END]);
 	}
 
 }
 	
-int wordCount(char *text){
+int wordCount(char *text, int length){
 
     int counter=0;
     int i=0;
+    float completion = 0;
     while(text[i] != '\0'){
         if (text[i] == ' '){
             counter++;
         }
         i++;
+        completion = ((float)i) / length;
+        progressBar((int) (completion * 100));
+
     }
+    printf("\n");
     return counter + 1;
 }
 
@@ -104,4 +113,35 @@ int validateFile(char *fileName) {
 		return 0;
 	}
 	
+}
+
+long fileLength(FILE *file) {
+
+	fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return length;
+}
+
+void progressBar(int rate) {
+
+	if (rate >= 100) {
+		rate = 100;
+	}
+	int numberOfBars = (rate / 10) * 3;
+	int totalNumberOfBars = 30;
+	int i;
+	fflush(stdout);
+	printf("\rProgress: [");
+
+	for (i = 0; i < numberOfBars; i++)
+	{
+		printf("#");
+		usleep(5);
+	}
+	for (i = 0; i < totalNumberOfBars - numberOfBars; i++)
+	{
+		printf(" ");
+	}
+	printf("] %d%%",rate);
 }
